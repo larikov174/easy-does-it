128cb5fa32daaeb14e9c8e005abe1340485ce8f1
Andrey Larikov <larikov174@gmail.com>
2022-03-21 16:04:55 +0500

    feat: code refactoring

---
+++ src/utils/const.js
@@ -0,0 +1,17 @@
+const DIALOG = {
+  START: "Click to start game",
+  WIN: "You win! (◕‿◕)",
+  LOSE: "You loose (ಥ﹏ಥ)"
+}
+
+const MAIN = {
+  TITLE: "Easy does it.",
+  SUBTITLE: "Rules:",
+  INFO_1: "Move when green light and stop before red light is on.",
+  INFO_2: "Get to the top of the field to win this game."
+}
+
+export {
+  DIALOG,
+  MAIN
+}
