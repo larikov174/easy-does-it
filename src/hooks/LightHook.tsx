7f3896deb08f9d0fa5e1fbfaef20295b6b31af66
Andrey Larikov <larikov174@gmail.com>
2022-03-22 14:51:12 +0500

    fix: tuneUp light flip timer

--- src/hooks/LightHook.tsx
+++ src/hooks/LightHook.tsx
@@ -7,7 +7,7 @@ const useLight = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       flipLight();
-    }, Math.floor(Math.random() * 2500));
+    }, Math.floor(Math.random() * 3500));
 
     return () => clearTimeout(timeout);
   }, [isGreen]);
