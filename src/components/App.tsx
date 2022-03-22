3a2077e9b49d6dea936d456b07c64ee34b8cc4ab
Andrey Larikov <larikov174@gmail.com>
2022-03-22 11:50:09 +0500

    feat: change design

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,34 +3,48 @@ import styled from "styled-components";
 import Light from "./Light";
 import Player from "./Player";
 import { DIALOG, MAIN } from '../utils/const';
+import Squid from '../images/squid.svg';
+import Guard from '../images/guard.png';
 
-const StyledLayout = styled.div`
+const Layout = styled.div`
   max-width: 100%;
   max-height: 100%;
   width: 100vw;
   height: 100vh;
   margin: 0 auto;
   background-color: #077687;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 640px) {
+    flex-direction: column;
+    justify-content: flex-start;
+    height: 100%;
+  }
 `;
 
 const MainContaier = styled.div`
   max-width: 320px;
-  height: 50%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
+  margin: 0 10px;
 `;
 
 const PlayField = styled.div`
   width: 320px;
   height: 320px;
   margin: 0;
+  border: 1px dotted white;
+
+  @media screen and (max-width: 640px) {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
-const StyledText = styled.p`
+const Text = styled.p`
   margin: 0;
   padding: 0;
   color: #fff;
@@ -39,46 +53,61 @@ const StyledText = styled.p`
   -moz-osx-font-smoothing: grayscale;
 `;
 
-const StyledTitle = styled(StyledText)`
+const Title = styled(Text)`
   padding-bottom: 5px;
   border-bottom: 1px solid grey;
   text-align: center;
   font-size: 24px;
 `;
 
-const StyledSubtitle = styled(StyledText)`
+const Subtitle = styled(Text)`
   margin-top: 60px;
   font-size: 20px;
 `;
 
-const StyledInfo = styled(StyledText)`
+const Info = styled(Text)`
   margin-top: 10px;
 `;
 
-const StyledNotifications = styled(StyledText)`
+const Notifications = styled(Text)`
   padding-top: 5px;
   border-top: 1px solid grey;
   text-align: center;
+`;
+
+const Decor = styled.img.attrs((props) => ({
+  src: props.src,
+  alt: props.alt || "",
+}))`
+  width: 130px;
+  height: auto;
+  margin: 0 10px;
+
+  @media screen and (max-width: 640px) {
+    width: 90px;
+    margin: 20px;
+  }
 `;
 
 const Main: React.FC = () => {
   const { gameState, onClick, isGreen, yCoordinate } = useApp();
 
   const gameMessageDiv = (
-    <StyledNotifications>
+    <Notifications>
       {gameState === GameState.IN_GAME 
         ? DIALOG.START 
         : gameState === GameState.WIN 
           ? DIALOG.WIN 
           : DIALOG.LOSE
       }
-    </StyledNotifications>
+    </Notifications>
   )
 
   return (
-    <StyledLayout>
+    <Layout>
+      <Decor src={Squid} />
       <MainContaier>
-        <StyledTitle>{MAIN.TITLE}</StyledTitle>
+        <Title>{MAIN.TITLE}</Title>
         <PlayField onClick={onClick}>
           <svg width="320" height="320">
             <Light isGreen={isGreen} />
@@ -86,11 +115,12 @@ const Main: React.FC = () => {
           </svg>
           {gameMessageDiv}
         </PlayField>
-        <StyledSubtitle>{MAIN.SUBTITLE}</StyledSubtitle>
-        <StyledInfo>{MAIN.INFO_ONE}</StyledInfo>
-        <StyledInfo>{MAIN.INFO_TWO}</StyledInfo>
+        <Subtitle>{MAIN.SUBTITLE}</Subtitle>
+        <Info>{MAIN.INFO_ONE}</Info>
+        <Info>{MAIN.INFO_TWO}</Info>
       </MainContaier>
-    </StyledLayout>
+      <Decor src={Guard} />
+    </Layout>
   );
 };
 export default Main;
